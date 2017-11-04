273bfd2112c66a755472c50b78f632f561f84229
Blacker <blacker@aliyun.com>
2017-11-05 07:33:10 +0800

    106

---
+++ 106/main/section-2/practice-1.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function countSameElements(collection) {
+  var result = [],j = 0,all = {},number= [];
+  for (var i=0;i<collection.length;i++){
+    all["name"] = collection[j];
+    number=collection.indexOf(collection[j]);
+    for (var z = 0;z<number.length;z++){
+      if (number[])
+          all["summary"] =
+    }
+    console.log(all);
+    j++;
+    result.push(all);
+  }
+  return ;
+};
