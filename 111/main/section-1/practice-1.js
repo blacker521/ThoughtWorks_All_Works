b697d2210fa03eab687a8af222896a3302920ea8
Blacker <blacker@aliyun.com>
2017-11-25 12:27:32 +0800

    1

--- 111/main/section-1/practice-1.js
+++ 111/main/section-1/practice-1.js
@@ -1,15 +1,16 @@
 'use strict';
 
 module.exports = function test() {
-    let i, result = "";
-    for (i = 99; i > 1; i--) {
+    let i, result ="";
+    for (i = 99; i > 2; i--) {
             result += i + " bottles of beer on the wall, " + i + " bottles of beer.\n" +
                 "Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.\n"
     }
-    result +="1 bottle of beer on the wall, 1 bottle of beer.\n" +
+    result +="2 bottles of beer on the wall, 2 bottles of beer.\n" +
+        "Take one down and pass it around, 1 bottle of beer on the wall.\n" +
+        "1 bottle of beer on the wall, 1 bottle of beer.\n" +
         "Take one down and pass it around, no more bottles of beer on the wall.\n" +
         "No more bottles of beer on the wall, no more bottles of beer.\n" +
-        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-
+        "Go to the store and buy some more, 99 bottles of beer on the wall.";
     return result;
 };
