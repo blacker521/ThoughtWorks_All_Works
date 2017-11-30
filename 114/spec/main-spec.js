91e9bd1b8b6e989600a963dabe25b4bb2ca4208c
Blacker <blacker@aliyun.com>
2017-11-30 21:32:02 +0800

    114

--- 114/spec/main-spec.js
+++ 114/spec/main-spec.js
@@ -1,5 +1,23 @@
 const main = require('../main/main');
 
-describe('main()', () => {
 
+describe('main()', () => {
+    var j;
+    beforeEach(
+        j = "1234"
+    );
+
+
+    it('Wait input',function () {
+        
+
+    })
+    
+    
+    it('Compared to the input',function () {
+        
+    })
+
+
+    
 });
