8cacc76c76479c43132fdb0334bb6d7fe52b4b51
Blacker <blacker@aliyun.com>
2017-12-23 13:32:30 +0800

    116

---
+++ 116/lib/database.js
@@ -0,0 +1,31 @@
+exports.loadStu = function () {
+    return [
+        {
+            name: "aaa",
+            stuNum: "001",
+            class: "1",
+            math: 0,
+            chinese: 0,
+            englinsh: 0,
+            computer: 0,
+        },
+        {
+            name: "bbb",
+            stuNum: "002",
+            class: "2",
+            math: 0,
+            chinese: 0,
+            englinsh: 0,
+            computer: 0,
+        },
+        {
+            name: "ccc",
+            stuNum: "003",
+            class: "1",
+            math: 0,
+            chinese: 0,
+            englinsh: 0,
+            computer: 0,
+        },
+    ];
+};
